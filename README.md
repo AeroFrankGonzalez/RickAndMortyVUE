f58bea568d6c33ae5dd0c073228dffb1f37bf3fd
AeroFrankGonzalez <117693098+AeroFrankGonzalez@users.noreply.github.com>
2023-03-11 09:40:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RickAndMortyVUE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-yhxi4g)
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="https://vuejs.org/images/logo.png" /> -->
+    <RouterView />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<footer
+  class="
+    fixed
+    bottom-0
+    left-0
+    w-full
+    flex
+    items-center
+    justify-center
+    bg-slate-900
+    py-2
+  "
+>
+  <RouterLink
+    to="/"
+    class="px-4 py-2 rounded-lg bg-cyan-400 shadow-lg shadow-lime-500/50 border-2 border-black outline outline-1 outline-white">
+    Home
+  </RouterLink>
+</footer>
